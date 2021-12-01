b14e028be1f187c354a186a1b112208584efaf5d
Abhishek <Abhishek123-code@user.noreply.github.com>
2021-12-01 11:52:58 +0530

    added files in Cprac and made a web development folder

---
+++ C prac/prog9.c
@@ -0,0 +1,15 @@
+/* 
+WAP to allow the user to play the game of rock paper scissors times with the computer.Log the scores of computer and the player.
+Display the name of the winner at the end.
+NOTE:- you have to display the name of the player during the game.Take user name as input form the user.
+
+rock vs scissors - rock wins
+paper vs scissors - scissors wins
+paper vs rock - paper wins
+*/
+#include<stdio.h>
+int main()
+{
+    
+    return 0;
+}
