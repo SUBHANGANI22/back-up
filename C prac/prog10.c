1a1f96210229b96c8f5f8965d3cd2c553612c226
Abhishek <Abhishek123-code@user.noreply.github.com>
2021-12-04 15:07:17 +0530

    Added few files in web development folder and in C prac

---
+++ C prac/prog10.c
@@ -0,0 +1,4 @@
+/*
+WAP to perform matrix multiplication using 2d array to multiply two matrices.
+The number of columns of the first matrix should be equal to the number of rows in the second matrix.
+*/
